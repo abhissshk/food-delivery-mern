f47bbb3917d80fd17503ee6bd0188808ce04c86f
abhissshk <abhishek629460@gmail.com>
2026-04-06 21:06:12 +0530

    fix header text visibility

--- Fronted/src/component/Header/Header.jsx
+++ Fronted/src/component/Header/Header.jsx
@@ -2,26 +2,21 @@ import React from 'react'
 import "./Header.css" 
 import header_img from "../../assets/header_img.png"
 
-
 function Header() {
   return (
-     <>
-      <div 
+    <div 
       className='header'
-      style={{ backgroundImage: `url(${header_img})` }}  // ✅ dynamic image
-    ></div>
-    <div className='header'>
-        <div className="header-contents">
-            <h2>Order your favourite food here</h2>
-            <p>Choose from a diverse menu featuring a delectable array of dishes crafted with the finest ingredients and culinary exprestise. Our mission is to satisfy your cravings and elevate your dining experience,one delicious meal at a time</p>
-            <button>View Menu</button>
-
-        </div>
-      
+      style={{ backgroundImage: `url(${header_img})` }}
+    >
+      <div className="header-contents">
+        <h2>Order your favourite food here</h2>
+        <p>
+          Choose from a diverse menu featuring a delectable array of dishes crafted 
+          with the finest ingredients and culinary expertise.
+        </p>
+        <button>View Menu</button>
+      </div>
     </div>
-     
-     
-     </>
   )
 }
 
